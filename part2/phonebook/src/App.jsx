8c4416b97c139ab307a4b0c0abb352b91570c27e
mashalink <maria8link@gmail.com>
2024-02-08 14:33:53 +0200

    without debug

---
+++ part2/phonebook/src/App.jsx
@@ -0,0 +1,48 @@
+import { useState } from 'react'
+import Person from './components/Person.jsx'
+
+const App = () => {
+  const [persons, setPersons] = useState([]) 
+  const [newName, setNewName] = useState('a new person...')
+    
+  const addName = (event) => {
+    event.preventDefault()
+    const  personObject = {
+      name: newName,
+      id: persons.length + 1,
+    }
+
+    setPersons(persons.concat(personObject))
+    setNewName('')
+  }
+
+  const handleNoteChange = (event) => {
+    console.log(event.target.value)
+    setNewName(event.target.value)
+  }
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={addName}>
+        <div>
+          name: <input 
+                  value={newName}
+                  onChange={handleNoteChange}
+                />
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      <ul>
+        {persons.map(person =>
+          <Person key={person.id} person={person} />
+        )}
+      </ul>
+    </div>
+  )
+}
+
+export default App
