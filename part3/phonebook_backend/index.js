ffdc0bdea9d869017e2a056ee9acc59f0f8c442a
Maria <maria8link@gmail.com>
2025-11-27 00:10:52 +0200

    Exercises 3.7.-3.8.

--- part3/phonebook_backend/index.js
+++ part3/phonebook_backend/index.js
@@ -1,30 +1,24 @@
 const express = require("express");
+const morgan = require("morgan");
 const app = express();
 
 let persons = [
-  {
-    id: "1",
-    name: "Arto Hellas",
-    number: "040-123456",
-  },
-  {
-    id: "2",
-    name: "Ada Lovelace",
-    number: "39-44-5323523",
-  },
-  {
-    id: "3",
-    name: "Dan Abramov",
-    number: "12-43-234345",
-  },
-  {
-    id: "4",
-    name: "Mary Poppendieck",
-    number: "39-23-6423122",
-  },
+  { id: "1", name: "Arto Hellas", number: "040-123456" },
+  { id: "2", name: "Ada Lovelace", number: "39-44-5323523" },
+  { id: "3", name: "Dan Abramov", number: "12-43-234345" },
+  { id: "4", name: "Mary Poppendieck", number: "39-23-6423122" },
 ];
 
+// parse JSON first
 app.use(express.json());
+
+// custom token for body
+morgan.token("body", (req) => JSON.stringify(req.body));
+
+// log with body
+app.use(
+  morgan(":method :url :status :res[content-length] - :response-time ms :body")
+);
 
 app.get("/info", (request, response) => {
   const count = persons.length;
@@ -34,6 +28,10 @@ app.get("/info", (request, response) => {
     <p>Phonebook has info for ${count} people</p>
     <p>${date}</p>
   `);
+});
+
+app.get("/api/persons", (req, res) => {
+  res.json(persons);
 });
 
 app.get("/api/persons/:id", (request, response) => {
@@ -50,7 +48,6 @@ app.get("/api/persons/:id", (request, response) => {
 app.delete("/api/persons/:id", (request, response) => {
   const id = request.params.id;
   persons = persons.filter((person) => person.id !== id);
-
   response.status(204).end();
 });
 
@@ -68,6 +65,7 @@ app.post("/api/persons", (request, response) => {
       error: "name or number missing",
     });
   }
+
   if (persons.find((person) => person.name === body.name)) {
     return response.status(400).json({
       error: "name must be unique",
@@ -81,12 +79,7 @@ app.post("/api/persons", (request, response) => {
   };
 
   persons = persons.concat(person);
-
   response.json(person);
-});
-
-app.get("/api/persons", (req, res) => {
-  res.json(persons);
 });
 
 const PORT = 3001;
