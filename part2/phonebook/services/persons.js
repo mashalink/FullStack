05db0f183d1d42bb1a6b3c24721f81722af77588
Maria <maria8link@gmail.com>
2025-11-27 13:01:55 +0200

    fixed persons.js

--- part2/phonebook/services/persons.js
+++ part2/phonebook/services/persons.js
@@ -1,25 +1,29 @@
 import axios from "axios";
 
-const api = axios.create({ baseURL: "http://localhost:3001" });
+const api = axios.create({
+  baseURL: "http://localhost:3001/api/persons",
+});
+
+export default api;
 
 // for testing error state:
-// const api = axios.create({ baseURL: "http://localhost:3001", timeout: 1 });
+// const api = axios.create({ baseURL: "http://localhost:3001/api/persons", timeout: 1 });
 
 const getPersons = () => {
-  return api.get("/persons").then((r) => r.data);
+  return api.get("/").then((r) => r.data);
   //.then((data) => new Promise((res) => setTimeout(() => res(data), 1500))); // for testing loading state
 };
 
 const createPerson = (person) => {
-  return api.post("/persons", person).then((r) => r.data);
+  return api.post("/", person).then((r) => r.data);
 };
 
 const updatePerson = (id, person) => {
-  return api.put(`/persons/${id}`, person).then((r) => r.data);
+  return api.put(`/${id}`, person).then((r) => r.data);
 };
 
 const deletePersonById = (id) => {
-  return api.delete(`/persons/${id}`).then((r) => r.data);
+  return api.delete(`/${id}`).then((r) => r.data);
 };
 
 export { getPersons, createPerson, updatePerson, deletePersonById };
