093c2cd7fd41953757f361784e5f0fa230a6ec20
Maria <maria8link@gmail.com>
2026-01-29 09:57:57 +0200

    persist login with localStorage and add logout

--- part5/bloglist-frontend/src/App.jsx
+++ part5/bloglist-frontend/src/App.jsx
@@ -4,6 +4,8 @@ import LoginForm from './components/LoginForm'
 import blogService from './services/blogs'
 import loginService from './services/login'
 
+const STORAGE_KEY = 'loggedBlogappUser'
+
 const App = () => {
   const [blogs, setBlogs] = useState([])
 
@@ -11,10 +13,19 @@ const App = () => {
   const [password, setPassword] = useState('')
   const [user, setUser] = useState(null)
 
+  useEffect(() => {
+    const savedUserJSON = window.localStorage.getItem(STORAGE_KEY)
+    if (savedUserJSON) {
+      const user = JSON.parse(savedUserJSON)
+      setUser(user)
+    }
+  }, [])
+
   const handleLogin = async ({ username, password }) => {
   try {
     const loggedInUser = await loginService.login({ username, password })
     setUser(loggedInUser)
+    window.localStorage.setItem(STORAGE_KEY, JSON.stringify(loggedInUser))
     setUsername('')
     setPassword('')
   } catch {
@@ -22,6 +33,12 @@ const App = () => {
   }
 }
 
+const handleLogout = () => {
+    window.localStorage.removeItem(STORAGE_KEY)
+    setUser(null)
+    setUsername('')
+    setPassword('')
+  }
 
   useEffect(() => {
     const fetchBlogs = async () => {
@@ -48,7 +65,11 @@ const App = () => {
   return (
     <div>
       <h2>Blogs</h2>
-      <div>{user.name} logged in</div>
+      <div>{user.name} logged in
+      <button type="button" onClick={handleLogout}>
+          logout
+        </button>
+      </div>
       <br />
       {blogs.map((blog) => (
         <Blog key={blog.id} blog={blog} />
