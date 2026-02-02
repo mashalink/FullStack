3e7020aeb1ec56fa87cd3e546ded7c2b6c6d16a3
Maria <maria8link@gmail.com>
2026-02-02 12:27:18 +0200

    toggle blog details with view button

--- part5/bloglist-frontend/src/components/Blog.jsx
+++ part5/bloglist-frontend/src/components/Blog.jsx
@@ -1,7 +1,43 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from 'react'
+
+const Blog = ({ blog }) => {
+  const [showDetails, setShowDetails] = useState(false)
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5,
+  }
+
+  const toggleDetails = () => {
+    setShowDetails(prev => !prev)
+  }
+
+  return (
+    <div style={blogStyle} className="blog">
+      <div>
+        {blog.title}{' '}
+        <button type="button" onClick={toggleDetails}>
+          {showDetails ? 'hide' : 'view'}
+        </button>
+      </div>
+
+      {showDetails && (
+        <div className="blogDetails">
+          <div>{blog.url}</div>
+          <div>
+            likes {blog.likes}{' '}
+            <button type="button">like</button>
+            <br />
+            {blog.author}{' '}
+          </div>
+          {/* вроде позже info о пользователе и кнопка remove */}
+        </div>
+      )}
+    </div>
+  )
+}
 
 export default Blog
