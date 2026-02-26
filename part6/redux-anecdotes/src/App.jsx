efb9ab4f6b2f0dfb36175216158ca9db01ddf14d
Maria <maria8link@gmail.com>
2026-02-26 14:38:48 +0200

    Fetch anecdotes from backend on app startup (6.14)

--- part6/redux-anecdotes/src/App.jsx
+++ part6/redux-anecdotes/src/App.jsx
@@ -1,9 +1,28 @@
+import { useEffect } from 'react'
+import { useDispatch } from 'react-redux'
+import { setAnecdotes } from './reducers/anecdoteReducer'
+import anecdotesService from '../services/anecdotes'
+
 import AnecdoteForm from './components/AnecdoteForm'
 import AnecdoteList from './components/AnecdoteList'
 import Filter from './components/Filter'
 import Notification from './components/Notification'
 
 const App = () => {
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    anecdotesService
+      .getAll()
+      .then(anecdotes => {
+        dispatch(setAnecdotes(anecdotes))
+      })
+      .catch(err => {
+        console.error(err)
+        Notification.show('Failed to fetch anecdotes', 5)
+      })
+  }, [dispatch])
+
   return (
     <div>
       <h2>Anecdotes</h2>
--- part6/redux-anecdotes/src/reducers/anecdoteReducer.js
+++ part6/redux-anecdotes/src/reducers/anecdoteReducer.js
@@ -1,14 +1,5 @@
 import { createSlice } from '@reduxjs/toolkit'
 
-const anecdotesAtStart = [
-  'If it hurts, do it more often',
-  'Adding manpower to a late software project makes it later!',
-  'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
-  'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
-  'Premature optimization is the root of all evil.',
-  'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
-]
-
 const getId = () => (100000 * Math.random()).toFixed(0)
 
 const asObject = (anecdote) => ({
@@ -17,11 +8,9 @@ const asObject = (anecdote) => ({
   votes: 0
 })
 
-const initialState = anecdotesAtStart.map(asObject).sort((a, b) => b.votes - a.votes)
-
 const anecdoteSlice = createSlice({
   name: 'anecdotes',
-  initialState,
+  initialState: [],
   reducers: {
     voteAnecdote(state, action) {
 
@@ -36,9 +25,13 @@ const anecdoteSlice = createSlice({
       const content = action.payload
       state.push(asObject(content))
       state.sort((a, b) => b.votes - a.votes)
+    },
+
+    setAnecdotes(state, action) {
+      return action.payload.slice().sort((a, b) => b.votes - a.votes) 
     }
   }
 })
 
-export const { voteAnecdote, createAnecdote } = anecdoteSlice.actions
+export const { voteAnecdote, createAnecdote, setAnecdotes } = anecdoteSlice.actions
 export default anecdoteSlice.reducer
