bc262dab039303d08c9e4d9228e9577a2c5f2743
Maria <maria8link@gmail.com>
2026-03-06 12:40:51 +0200

    add individual user view 7.15

--- part7/bloglist-frontend-redux/src/components/User.jsx
+++ part7/bloglist-frontend-redux/src/components/User.jsx
@@ -1,9 +1,35 @@
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import usersService from "../services/users";
 
 const User = () => {
   const { id } = useParams();
+  const [user, setUser] = useState(null);
 
-  return <div>User id: {id}</div>;
+  useEffect(() => {
+    usersService.getAll().then((users) => {
+      const foundUser = users.find((u) => u.id === id);
+      setUser(foundUser);
+    });
+  }, [id]);
+
+  if (!user) {
+    return <div>loading...</div>;
+  }
+
+  return (
+    <div>
+      <h2>{user.name}</h2>
+
+      <h3>added blogs</h3>
+
+      <ul>
+        {user.blogs.map((blog) => (
+          <li key={blog.id}>{blog.title}</li>
+        ))}
+      </ul>
+    </div>
+  );
 };
 
 export default User;
